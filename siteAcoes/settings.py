caa2a29720944481e36ff8130b2bcf73bcd4b51f
Eduardo Klock de Lemos <eduardo.klock@gmail.com>
2017-07-13 14:18:19 -0300

    Static files path

--- siteAcoes/settings.py
+++ siteAcoes/settings.py
@@ -130,9 +130,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
